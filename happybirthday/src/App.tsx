fb3569740df4acd5388e3b60527408b0bbeba4e1
Luca Visentin <lucavisentin@nidektechnologies.it>
2025-06-18 13:02:01 +0200

    hb eleo pink v

--- happybirthday/src/App.tsx
+++ happybirthday/src/App.tsx
@@ -1,5 +1,15 @@
+import "./css/app.scss";
+
 function App() {
-  return <div className="hp">HB ELEO</div>;
+  return (
+    <div className="hb-eleo">
+      <div className="hb-eleo__container">
+        <div className="hb-eleo__title">
+          <h1>Happy Birthday Eleo!</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
