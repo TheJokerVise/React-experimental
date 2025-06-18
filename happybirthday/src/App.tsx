89a4c46fd57b8b7b75b7d680aba095749a80962b
Luca Visentin <lucavisentin@nidektechnologies.it>
2025-06-18 13:05:42 +0200

    big title

--- happybirthday/src/App.tsx
+++ happybirthday/src/App.tsx
@@ -3,8 +3,8 @@ import "./css/app.scss";
 function App() {
   return (
     <div className="hb-eleo">
-      <div className="hb-eleo__container">
-        <div className="hb-eleo__title">
+      <div className="hb-eleo-container">
+        <div className="hb-eleo-title">
           <h1>Happy Birthday Eleo!</h1>
         </div>
       </div>
