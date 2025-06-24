e8e122b1ed31780cb3fa1e9e9800a6f6aaad89cb
Luca Visentin <lucavisentin@nidektechnologies.it>
2025-06-24 21:22:28 +0200

    add backgrounds

--- happybirthday/src/App.tsx
+++ happybirthday/src/App.tsx
@@ -2,25 +2,15 @@ import { Marquee } from "./components/Marquee";
 import "./css/app.scss";
 
 function App() {
-  const iconsDogs: string[] = [
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-    "/icons/beagle1.svg",
-  ];
-
   return (
     <div className="hb-eleo">
       <div className="hb-eleo-container">
-        <Marquee icons={iconsDogs} reverse={true} />
         <Marquee message="Happy Birthday Eleo!" />
         <div className="hb-eleo-title">
-          <h1>Happy Birthday Eleo!</h1>
+          Lunedì 4 Agosto festeggio il mio primo compleanno!!!
+        </div>
+        <div className="hb-eleo-title">
+          Ti aspetto nel mio giardino a partire dalle 18:00
         </div>
       </div>
     </div>
